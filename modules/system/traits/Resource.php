add8aa32a45e30372ff37f211bbfc27effdcc7c0
Nosaraei <m.nosaraei@gmail.com>
2025-01-18 10:37:11 +0330

    Add basic filter scope to resource trait

--- modules/system/traits/Resource.php
+++ modules/system/traits/Resource.php
@@ -1,6 +1,7 @@
 <?php namespace System\Traits;
 
 use Illuminate\Database\Eloquent\Relations\Relation;
+use System\Helpers\Utility;
 
 trait Resource
 {
@@ -19,4 +20,48 @@ trait Resource
 
         return get_parent_class();
     }
+
+    public function scopeBasicFilter($query, $request){
+
+        $filters = Utility::getValidList($request->basic_filters);
+
+        foreach ($filters as $filter_key => $filter) {
+
+            if(method_exists(get_called_class(), "scope" . studly_case($filter_key))){
+
+                $query->{camel_case($filter_key)}($filter->values, $filter); // , ["filters" => $filters]
+
+            }
+            else{
+
+                switch ($filter->type) {
+                    case 'multi':
+                        $query->whereIn($filter_key, $filter->values);
+                        break;
+
+                    case 'boolean':
+                        $query->where($filter_key, $filter->values[0]);
+                        break;
+
+                    case 'less_than':
+                        $query->where($filter_key, '<=', $filter->values[0]);
+                        break;
+
+                    case 'more_than':
+                        $query->where($filter_key, '>=', $filter->values[0]);
+                        break;
+
+                    case 'between':
+                        $query->whereBetween($filter_key, $filter->values);
+                        break;
+
+                }
+
+            }
+
+        }
+
+        return $query;
+    }
+
 }
